873278c771f654793a65a103a19d49c96f8d064a
tom <tcuypers@gmail.com>
2020-03-07 18:30:21 +0100

    Update serial interface

--- backend/input_serial.py
+++ backend/input_serial.py
@@ -1,24 +1,21 @@
 import time
 from typing import Iterator
 from inputs import Inputs
-try:
-    from serial import Serial
-except:
-    pass
+from serial import Serial
 
 class InputSerial(Inputs):
     def __init__(self) -> None:
         self.stream = Serial(port='/dev/ttyS0')
 
-    def __del__(self) -> None:
-        self.stream.close()
-
     def get_data(self) -> Iterator[str]:
         data = ''
+        first_received = False
         while True:
-            c = self.stream.read(1)
-            if c != '|':
+            c = self.stream.read(1).decode('utf-8')
+            if c != '|' and first_received:
                 data += c
-            else:
+            elif first_received:
                 yield data
                 data = ''
+            else:
+                first_received = True
